14807315103b9e799293619d952eab58adb1dddc
harshit gururani <harshit10gururani7@gmail.com>
2025-04-01 19:48:41 +0530

    changes

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -21,6 +30,9 @@ app.use((0, cors_1.default)({
     credentials: true,
 }));
 app.use((0, express_2.clerkMiddleware)());
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.status(200).json({ message: "test data" });
+}));
 app.use("/api/document", document_1.default);
 app.get("/", validateRequest_1.validateRequest, (req, res) => {
     const auth = req.auth;
